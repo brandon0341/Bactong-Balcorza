9d3c5b2798d5ee90ebc02c6e5dcfb92bcee9c693
brandon0341 <150418589+brandon0341@users.noreply.github.com>
2023-12-20 21:32:32 +0800

    Add files via upload

---
+++ FinalOutput/src/main/java/com/mycompany/finaloutput/FinalOutput.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finaloutput;
+
+/**
+ *
+ * @author 63917
+ */
+public class FinalOutput {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
